d64129eabc02b8065dc4f308e42178200f2b4a32
Rohit Kumar <rohitkv090@gmail.com>
2023-07-03 19:12:59 +0530

    inital comming

---
+++ index.js
@@ -0,0 +1,31 @@
+const fastify = require('fastify')({
+
+});
+
+const port=process.env.PORT || 9292;
+
+
+fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: (req, res) => {
+        console.log(req.socket.remoteAddress);
+        console.log(req.ip)
+        console.log(req.ips)
+        console.log(req.hostname)
+        console.log(req.protocol)
+    }
+})
+
+const app = async () => {
+    await fastify.listen({
+        port: port,
+    });
+    console.log(`Server is listening on port ${port}`);
+}
+
+
+
+
+
+app();
